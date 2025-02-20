e79b04e4e5159dfd1ff1085e31d1f775baee6fad
Herik <herik.spada@aluno.senai.br>
2025-02-20 16:57:05 -0300

    :sparkles: Post: Enviando dados dos generos para os sites externos juntamente com o link

---
+++ Interfaces/IFilmeRepository.cs
@@ -0,0 +1,17 @@
+﻿using api_filmes_senai.Domains;
+
+namespace api_filmes_senai.Interfaces
+{
+    public interface IFilmeRepository
+    {
+        void Cadastar(Filme novoFilme);
+
+        List<Filme> Cadastar();
+
+        void Atualizar(Guid id, Filme filme);
+
+        void Deletar(Guid id);
+
+        Filme BuscarPorId(Guid id);
+    }
+}
